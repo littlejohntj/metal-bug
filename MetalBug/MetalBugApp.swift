442f9a1e863f132876e2293ae21d0f03c093fa12
littlejohntj <tjl13c@my.fsu.edu>
2023-10-17 14:58:37 -0400

    Initial Commit

---
+++ MetalBug/MetalBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalBugApp.swift
+//  MetalBug
+//
+//  Created by Todd Littlejohn on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MetalBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
